4f94115194b2bd291c8eade7db5e58c95d585f96
rezhanurludvi <muhammad.nurludvi@students.amikom.ac.id>
2021-10-19 21:44:25 +0700

    first commit

---
+++ resources/views/Template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+     RezhaID
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021 <a href="https://adminlte.io">Muhammad Rezha Nurludvi</a>.</strong> All rights reserved.
+  </footer>
